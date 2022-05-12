2dacec714898f188ef53aa848dc570c26853e568
Islam <50731444+xislam@users.noreply.github.com>
2022-05-12 12:37:09 +0600

    create model News and regicter in admin News and create migration for News

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,22 @@
+from ckeditor.widgets import CKEditorWidget
+from django import forms
 from django.contrib import admin
 
+from news.models import News
+
 # Register your models here.
+
+
+class NewsAdminForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = News
+        fields = "__all__"
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
+
+
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,1 +1,18 @@
+from ckeditor.fields import RichTextField
 from django.db import models
+from django.utils.translation import gettext as _
+
+
+class News(models.Model):
+    """Model for News"""
+
+    title = models.CharField(max_length=125, verbose_name=_("Title"))
+    img = models.ImageField(verbose_name=_("Photo for news"))
+    text = RichTextField()
+    date_create = models.DateTimeField(verbose_name=_("Create date time"))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News"
