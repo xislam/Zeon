b5ac2bd198578d2dd88213fe54f518f830862a3a
Islam <50731444+xislam@users.noreply.github.com>
2022-06-12 21:20:13 +0600

    test

--- feedback/views.py
+++ feedback/views.py
@@ -1,14 +1,9 @@
-from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
+from feedback.models import Feedback
 from feedback.serializers import FeedbackSerializer
 
 
-class FeedbackCreate(viewsets.ViewSet):
-    def create(self, request, format=None):
-        serializer = FeedbackSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class FeedbackCreate(ModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
