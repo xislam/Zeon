1662dd5ed85e9f3ee7aabb89235e27d7b04c9882
Islam <50731444+xislam@users.noreply.github.com>
2022-06-16 18:32:37 +0600

    commiting cod in admin.py

--- partner_site/admin.py
+++ partner_site/admin.py
@@ -1,20 +1,20 @@
-from django.contrib import admin
-
-from partner_site import models
-
-
-class AnswerInlineModel(admin.TabularInline):
-    models
-    fields = "__all__"
-
-
-@admin.register(models.ContactUs)
-class QuestionAdmin(admin.ModelAdmin):
-    fields = [
-        "title",
-        "quiz",
-    ]
-    list_display = ["title", "quiz", "date_updated"]
-    inlines = [
-        AnswerInlineModel,
-    ]
+# from django.contrib import admin
+#
+# from partner_site import models
+#
+#
+# class AnswerInlineModel(admin.TabularInline):
+#     models
+#     fields = "__all__"
+#
+#
+# @admin.register(models.ContactUs)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = [
+#         "title",
+#         "quiz",
+#     ]
+#     list_display = ["title", "quiz", "date_updated"]
+#     inlines = [
+#         AnswerInlineModel,
+#     ]
