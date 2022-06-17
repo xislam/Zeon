3d9de3522386bd65eff213fb444c0133021239ee
Islam <50731444+xislam@users.noreply.github.com>
2022-06-17 15:04:05 +0600

    update urls

--- partner_site/urls.py
+++ partner_site/urls.py
@@ -10,7 +10,7 @@ router.register(
     r"social_network/list/", views.SocialNetworkList, basename="SocialNetwork"
 )
 router.register(r"direction/list/", views.DirectionList, basename="Direction")
-router.register(r"question_cv/list/", views.ContactUsCreate, basename="QuestionCVList")
+router.register(r"question_cv/list/", views.QuestionCVList, basename="QuestionCVList")
 router.register(r"answer/list/", views.AnswerList, basename="AnswerList")
 router.register(r"cv/create/", views.PartnerCVCreate, basename="PartnerCV")
 
