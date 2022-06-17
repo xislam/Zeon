3aa3c3257ea531f1e8e9840d1eaf43b7ab95b6fe
Islam <50731444+xislam@users.noreply.github.com>
2022-06-17 15:21:13 +0600

    update urls

--- partner_site/urls.py
+++ partner_site/urls.py
@@ -7,12 +7,12 @@ from partner_site import views
 router = routers.DefaultRouter()
 router.register(r"contact_us/", views.ContactUsCreate, basename="ContactUs")
 router.register(
-    r"social_network/list/", views.SocialNetworkList, basename="SocialNetwork"
+    r"social_network/list", views.SocialNetworkList, basename="SocialNetwork"
 )
-router.register(r"direction/list/", views.DirectionList, basename="Direction")
-router.register(r"question_cv/list/", views.QuestionCVList, basename="QuestionCVList")
-router.register(r"answer/list/", views.AnswerList, basename="AnswerList")
-router.register(r"cv/create/", views.PartnerCVCreate, basename="PartnerCV")
+router.register(r"direction/list", views.DirectionList, basename="Direction")
+router.register(r"question_cv/list", views.QuestionCVList, basename="QuestionCVList")
+router.register(r"answer/list", views.AnswerList, basename="AnswerList")
+router.register(r"cv/create", views.PartnerCVCreate, basename="PartnerCV")
 
 app_name = "partner_site"
 
